7959818b7bc66503007c7d49d29b00d4043f09e5
Richard Reveron <rwakos@gmail.com>
2025-06-12 11:05:46 +0200

    cleanup

--- src/config/app-config.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.config = void 0;
-exports.config = {
-    suits: ['hearts', 'diamonds', 'clubs', 'spades'],
-    ranks: ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'],
-    MAX_PLAYERS: 8,
-};
